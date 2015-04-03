20fa84d1193c87f67a6d0aecde90304f09fb36d0
profiks <denys.ark@gmail.com>
2015-04-03 13:37:59 +0300

    Code Igniter base

---
+++ database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `feeds` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `link` varchar(255) DEFAULT NULL,
+  `thumbnail` varchar(255) DEFAULT NULL,
+  `description` text,
+  `users_id` int(11) unsigned NOT NULL,
+  `favourite` tinyint(1) DEFAULT NULL,
+  `views` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+
+
+CREATE TABLE IF NOT EXISTS `rss_posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `link` varchar(255) DEFAULT NULL,
+  `feeds_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_rss_posts_feeds1` (`feeds_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
